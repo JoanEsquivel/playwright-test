f7a7789917e9484c63b85bb87c9f11e05dabc15f
Joan Esquivel <48256183+JoanEsquivel@users.noreply.github.com>
2024-03-24 11:57:46 -0600

    feat: add shard example

--- playwright.config.js
+++ playwright.config.js
@@ -17,7 +17,7 @@ dotenv.config();
 module.exports = defineConfig({
   testDir: './tests',
   /* Run tests in files in parallel */
-  // fullyParallel: true,
+  fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
@@ -25,7 +25,7 @@ module.exports = defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: process.env.CI ? 'blob' : 'html',                                                                                                                 
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
--- tests/parallelism-folder/test-file-2.spec.js
+++ tests/parallelism-folder/test-file-2.spec.js
@@ -1,18 +1,22 @@
 // @ts-check
 const { test, expect } = require('@playwright/test');
-
+let timeout = 0;
 test('parallel - TF #2 - TC #1 ', async ({ page }) => {
     console.log('parallel - TF #2 - TC #1')
+    await page.waitForTimeout(timeout);
 });
 
 test('parallel - TF #2 - TC #2', async ({ page }) => {
     console.log('parallel - TF #2 - TC #2')
+    await page.waitForTimeout(timeout);
 });
 
 test('parallel - TF #2 - TC #3', async ({ page }) => {
     console.log('parallel - TF #2 - TC #3')
+    await page.waitForTimeout(timeout);
 });
 
 test('parallel - TF #2 - TC #4', async ({ page }) => {
     console.log('parallel - TF #2 - TC #4')
+    await page.waitForTimeout(timeout);
 });
